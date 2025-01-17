ecd28714833a60b78638a6ae0dd87e24132128c8
Abishevs <eduards@abishevs.dev>
2025-01-17 02:13:44 +0100

    feat: Got http Header method and URL parsed.

--- src/main.c
+++ src/main.c
@@ -8,40 +8,101 @@
 
 #define MAX_CONNECTIONS 5
 #define BUFFER_SIZE 1024
+#define PATH_SIZE 256
+#define METHOD_MAX_BYTES 5 // 5 bytes max
 #define PORT 6969
 
 typedef enum Method 
 {
     GET,
     POST,
+    INVALID
 } Method;
 
 typedef struct Header  
 {
     Method method;
-    char *path;
-    char *msg;
+    char path[PATH_SIZE];
+    char msg;
 } Header;
+
+void set_method(Header *h, char *method)
+{
+    if (strcmp(method, "GET") == 0) {
+        h->method = GET;
+    } else if (strcmp(method, "POST" ) == 0) {
+        h->method = POST;
+    }  else {
+        h->method = INVALID;
+    }
+}
 
 int handler(char buf[BUFFER_SIZE], int client_fd)
 {
-    char method[256];
-    int i = 0;
+    Header header;
+    memset(&header, 0, sizeof(Header));
+    
+    char value[PATH_SIZE] = {0};
+    int value_counter = 0;
+    int value_len = 0;
+    char *msg;
 
-    while (i < BUFFER_SIZE && buf[i] != '\n' && i < 255) {
-        method[i] = buf[i];
-        i++;
+    for (int i = 0; buf[i] != '\0' && i < BUFFER_SIZE; i++) {
+        if (buf[i] == ' ') {
+/*          When we found space, value is ready for use */
+/*          Null-terminate the method string */
+            printf("Space found at index %d\n", i);
+            value[value_len] = '\0';
+
+            if (value_counter == 0){
+                // memset(value, 0, sizeof(value)); /* CLR VALUE */
+                set_method(&header, value);
+                printf("value: %s, METHOD: %d\n", value, header.method);
+                if (header.method == INVALID) {
+                /* TODO: Throw 400 Bad Request Instantlyy */
+                    msg = "HTTP/1.1 400 Bad Request\r\nContent-Length: 4\r\n\r\nBAD:";
+                    send(client_fd, msg, strlen(msg), 0);
+                    fprintf(stderr,"%s",msg);
+                    return -1;
+                }
+
+
+            } else if (value_counter == 1) {
+                strcpy(header.path, value);
+                printf("value: %s, PATH: %s\n", value, header.path);
+                /*          Only intrested in these two rn, so break out */
+                break;
+            }
+            value_counter ++;
+            value_len = 0;
+        } else {
+            value[value_len++] = buf[i];
+        }
+
     }
 
-    // Null-terminate the method string
-    method[i] = '\0';
+    printf("METHOD: %d, PATH: %s", header.method, header.path);
+   //  char *welcome_message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+   // 
+   //  
+   //  send(client_fd, welcome_message, strlen(welcome_message), 0);
+    msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 1000\r\n\r\n<html> <head><title>My Page</title></head> <body>Welcome to my website!</body> </html>";
 
-    printf("First line: %s\n", method);
+    send(client_fd, msg, strlen(msg), 0);
     return 1;
 }
 
-int main() 
+int main(int argc, char *argv[]) 
 {
+    char *root_dir = NULL;
+
+    if (argc == 2) {
+        root_dir = argv[1];
+    } else {
+        root_dir = argv[0];
+    }
+
+    printf("Root dir: '%s'\n", root_dir);
 
     char buf[BUFFER_SIZE];
     struct sockaddr_in server_addr, client_addr;
@@ -88,16 +149,13 @@ int main()
         printf("DEBUG: New connection from %s:%d\n",
                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-        // char *welcome_message = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-        // 
-        //
-        // send(client_fd, welcome_message, strlen(welcome_message), 0);
+
         ssize_t bytes_received = recv(client_fd, buf, BUFFER_SIZE - 1, 0);
         if (bytes_received < 0) {
             perror("Receive failed");
         } else {
-            buf[bytes_received] = '\0'; // Null-terminate the received data
-            printf("Received: %s\n", buf);
+            // buf[bytes_received] = '\0'; // Null-terminate the received data
+            printf("Received:%s\n", buf);
             handler(buf, client_fd);
         }
         
